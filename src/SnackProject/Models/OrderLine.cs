c561062265ec195d97dbd453c6a4d6ead3767eac
Kerberross <denisrob23@gmail.com>
2016-11-15 14:25:48 +0100

    Création des modèles du snack (TODO:many to many orderLine Crudity)

---
+++ src/SnackProject/Models/OrderLine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SnackProject.Models
+{
+    public class OrderLine
+    {
+        public int ID { get; set; }
+        public int quantity { get; set; }
+
+        //many to many a ajouter
+        public ICollection<Crudity> Crudities { get; set; }
+
+        public Sandwich Sandwichs { get; set; }
+    }
+}
