2b4584fad6c175e3ffb1d2324fd41a3906fa3b8f
jerome-9132 <debotj@gmail.com>
2016-12-07 10:26:21 +0100

    Bug inscription fix, model restriction employee little fix and more

--- src/ClientProject/Controllers/CompanyRegisterController.cs
+++ src/ClientProject/Controllers/CompanyRegisterController.cs
@@ -66,7 +66,7 @@ namespace ClientProject.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
+        public async Task<IActionResult> Index(RegisterViewModel model, string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
             if (await _activationInformant.IsRegistered())
