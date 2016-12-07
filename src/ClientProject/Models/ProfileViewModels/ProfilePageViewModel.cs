994013a9999fa5c5ff2237e300c99e2f0029f7ea
van-y_000 <van-y_000@PORTABLE-YOAN>
2016-12-07 14:37:36 +0100

    suite gui 14

---
+++ src/ClientProject/Models/ProfileViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientProject.Models.ProfileViewModels
+{
+    public class ProfilePageViewModel
+    {
+        private Employee employee { get; set; }
+        private List<Order> orderList { get; set; }
+        public ProfilePageViewModel(Employee employee, List<Order> orderList)
+        {
+            this.employee = employee;
+            this.orderList = orderList;
+        }
+    }
+}
