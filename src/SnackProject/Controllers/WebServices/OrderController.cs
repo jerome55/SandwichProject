0f926a19cfb458190ca433bcaf26562279664021
jerome-9132 <debotj@gmail.com>
2016-12-22 15:23:25 +0100

    I hate when i have to commit just to checkout

--- src/SnackProject/Controllers/WebServices/OrderController.cs
+++ src/SnackProject/Controllers/WebServices/OrderController.cs
@@ -14,7 +14,7 @@ namespace SnackProject.Controllers.WebServices
     [Route("api/Order")]
     public class OrderController : Controller
     {
-        public static Dictionary<string, Order> orderToValidate = new Dictionary<string, Order>();
+        public static Dictionary<string, OrderCompany_Com> orderToValidate = new Dictionary<string, OrderCompany_Com>();
         ApplicationDbContext _context;
 
         public OrderController(ApplicationDbContext context)
@@ -23,7 +23,97 @@ namespace SnackProject.Controllers.WebServices
         }
 
 
+        /**
+         * Au moment de confirmer son panier, le client envoye son objet Order qui correspond au panier ainsi que 
+         * l'objet Company correspondant à l'entreprise à laquelle il appartient.
+         * 
+         * Les informations de dates et de prix du coté client ne sont peut être pas correctes ou à jour. Ainsi,
+         * ce sont les informations du coté serveur qui font foi.
+         * 
+         * Un objet Order corrigé avec les informations du serveur est renvoyé au client pour qu'ils aient tous deux
+         * la même version des données. Ce même objet est aussi stocké sur le serveur en attente d'une réponse du client.
+         * 
+         * Après avoir effectué ses propres vérifications de son coté (crédits suffisants), le client confirme 
+         * définitivement ou abandonne la commande.
+         *                                                                  ------
+         *  Client  ---------------Company, Order --------------)  Serveur       |
+         *  Client  (--------------Updated_Order-----------------  Serveur       |--> [Post] api/Order
+         *                                                                       |
+         *       -> Order stockée temporaire en attente d'une validation client  |
+         *                                                                  ------                                                            
+         *                                                                  
+         * [IF-                                                                 
+         *  -OK](Crédits suffisants)                                        ------
+         *  Client  ------------{{GUID_Of_Order}OK}-------------)  Serveur       |
+         *                                                                       |
+         *                -> L'Order stockée est définitvement sauvegardé en DB  | 
+         *                                                                       |--> [Post] api/Order/Confirm
+         *  -KO](Crédits insuffisants)                                           |
+         *  Client  ------------{{_____________}KO}-------------)  Serveur       |
+         *                -> L'Order stockée est abandonnée (ou timeout)         |
+         *                                                                  ------
+         **/
         [HttpPost]
+        public async Task<CommWrap<OrderCompany_Com>> Post([FromBody]OrderCompany_Com communication)
+        {
+            Order orderClient = communication.Order_com;
+            Company company = communication.Company_com;
+
+            Company companyDB = null;
+            List<Company> companyList = _context.Companies.Where(q => q.Id == company.Id && q.Chkcode == company.Chkcode).ToList();
+
+            if (companyList.Count == 0)
+            {
+                return new CommWrap<OrderCompany_Com> { RequestStatus = 0 };
+            }
+            else
+            {
+                companyDB = companyList.First();
+
+                //Déterminer la date de la commande (seule la date server-side fait foi) 
+                DateTime now = DateTime.Now;
+                DateTime deliveryDate;
+                if (now.Hour >= 10)
+                {
+                    deliveryDate = DateTime.Today.AddDays(1.0);
+                }
+                else
+                {
+                    deliveryDate = DateTime.Today;
+                }
+
+                orderClient.DateOfDelivery = deliveryDate;
+
+                //Rafraichir les anciennes données avec des nouvelles par mesure de sécurité (les commandes 
+                //aux alentours de 10h sont susceptibles d'être altérées)
+                using (var dbContextTransaction = _context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        for (int i = 0; i < orderClient.OrderLines.Count; i++)
+                        {
+                            orderClient.OrderLines.ElementAt(i).Sandwich.Price = _context.Sandwiches.Where(s => s.Id == orderClient.OrderLines.ElementAt(i).Sandwich.Id).First().Price;
+                            orderClient.OrderLines.ElementAt(i).Sandwich.Name = _context.Sandwiches.Where(s => s.Id == orderClient.OrderLines.ElementAt(i).Sandwich.Id).First().Name;
+                            orderClient.OrderLines.ElementAt(i).Sandwich.Description = _context.Sandwiches.Where(s => s.Id == orderClient.OrderLines.ElementAt(i).Sandwich.Id).First().Description;
+
+                            for (int j = 0; j < orderClient.OrderLines.ElementAt(i).OrderLineVegetables.Count; j++)
+                            {
+                                orderClient.OrderLines.ElementAt(i).VegetablesPrice = _context.Menus.First().VegetablesPrice;
+                                orderClient.OrderLines.ElementAt(i).OrderLineVegetables.ElementAt(j).Vegetable.Name = _context.Vegetables.Where(v => v.Id == orderClient.OrderLines.ElementAt(i).OrderLineVegetables.ElementAt(j).Vegetable.Id).First().Name;
+                                orderClient.OrderLines.ElementAt(i).OrderLineVegetables.ElementAt(j).Vegetable.Description = _context.Vegetables.Where(v => v.Id == orderClient.OrderLines.ElementAt(i).OrderLineVegetables.ElementAt(j).Vegetable.Id).First().Description;
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        return new CommWrap<OrderCompany_Com> { RequestStatus = 0 };
+                    }
+                
+                return new CommWrap<OrderCompany_Com> { RequestStatus = 1, Content = communication};
+            }
+        }
+
+        /*[HttpPost]
         public async Task<CommWrap<OrderGuid_Com>> Post([FromBody]OrderCompany_Com communication)
         {
             Order orderAdd = communication.Order_com;
@@ -82,9 +172,9 @@ namespace SnackProject.Controllers.WebServices
                         orderAdd.OrderLines.ElementAt(i).OrderLineVegetables.ElementAt(j).Vegetable.Name = _context.Vegetables.Where(v => v.Id == orderAdd.OrderLines.ElementAt(i).OrderLineVegetables.ElementAt(j).Vegetable.Id).First().Name;
                         orderAdd.OrderLines.ElementAt(i).OrderLineVegetables.ElementAt(j).Vegetable.Description = _context.Vegetables.Where(v => v.Id == orderAdd.OrderLines.ElementAt(i).OrderLineVegetables.ElementAt(j).Vegetable.Id).First().Description;
                     }
-               
+
                     //Ajouter chaque OrderLine de l'object Order reçu par le serveur
-                    orderDB.AddOrderLine()
+                    orderDB.AddOrderLine();
                 }
 
                 orderDB.AddOrderLine(orderLineAddList);
@@ -93,7 +183,7 @@ namespace SnackProject.Controllers.WebServices
 
                 return new CommWrap<Order> { RequestStatus = 1, Content = orderDB };
             }
-        }
+        }*/
 
         [HttpPost]
         [Route("api/Order/Confirm")]
